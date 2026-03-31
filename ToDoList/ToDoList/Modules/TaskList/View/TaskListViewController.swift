bb67afc78fa8fa29df15d1a1766d67328786f234
merinovvvv <87500057+merinovvvv@users.noreply.github.com>
2026-03-31 12:56:09 +0300

    Set up initial project structure

---
+++ ToDoList/ToDoList/Modules/TaskList/View/TaskListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Yaraslau Merynau on 31.03.26.
+//
+
+import UIKit
+
+final class TaskListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
